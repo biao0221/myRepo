525fe2e4d85e0f4e5ecce191e0cbab62cf25ac6a
biao0221 <1103772125@qq.com>
2020-04-15 14:28:36 +0800

    second commit

--- src/main/java/webadv/s99201105/p02/App.java
+++ src/main/java/webadv/s99201105/p02/App.java
@@ -1,8 +1,26 @@
 package webadv.s99201105.p02;
 
 import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        String account = "17204213";
+        String password = "12345";
+        String lockpw = sha256hex(password);
+        System.out.println("账号：");
+        Scanner in = new Scanner(System.in);
+        String myAccount =  in.next();
+        System.out.println("密码：");
+        String myPassword =  in.next();
+        String lockMyPw = sha256hex(myPassword);
+        if(account.equals(myAccount)&&lockpw.equals(lockMyPw)){
+            System.out.println("登录成功");
+        }else {
+            System.out.println("登录失败");
+        }
+
         if (args.length < 1) {
             System.err.println("Please provide an input!");
             System.exit(0);
